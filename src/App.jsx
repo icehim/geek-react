0bd37cadffc28acae1e6f1e52390934145c2c4e5
zhiming <zhiming.hao@qq.com>
2022-12-07 20:36:31 +0800

    基础路由配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css';
+import './App.scss';
+
+//BrowserRouter(history),HashRouter(hash模式)
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+//导入页面组件
+import NotFound from "./pages/404";
+import Login from "./pages/login";
+import Layout from "./pages/layout";
 
 function App() {
     return (
-        <div className="app">
-            app
-        </div>
+        <BrowserRouter>
+            <div className="app">
+                {/*配置路由规则*/}
+                <Switch>
+                    <Route path='/home' component={Layout}/>
+                    <Route path='/login' component={Login}/>
+                    {/*配置404=>必须放到最后*/}
+                    <Route component={NotFound}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
