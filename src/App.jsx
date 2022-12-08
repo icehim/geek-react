02ecd0b308e34902f662c2ff7c7d82f37f231e91
zhiming <zhiming.hao@qq.com>
2022-12-08 22:51:45 +0800

    利用render实现路由拦截

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import Layout from "@/pages/layout";
 import Login from "@/pages/login";
 import NotFound from "@/pages/404";
+import {isAuth} from "@/utils/auth";
 
 function App() {
     return (
@@ -15,7 +16,20 @@ function App() {
                 {/*配置路由规则*/}
                 <Switch>
                     <Redirect exact from='/' to='/home'/>
-                    <Route path='/home' component={Layout}/>
+                    {/*<Route path='/home' component={Layout}/>*/}
+                    <Route path='/home' render={() => {
+                        /*
+                        * 需求:根据是否有token,决定是否能访问某个页面
+                        * 1.有token,返回Layout组件
+                        * 2.没有token,使用Redirect组件重定向跳转到login登录页
+                        * */
+                        if (!isAuth()) {
+                            //没有登录
+                            return <Redirect to='/login'/>
+                        }
+                        //已经登录=》正常显示页面
+                        return <Layout/>
+                    }}/>
                     <Route path='/login' component={Login}/>
                     {/*配置404=>必须放到最后*/}
                     <Route component={NotFound}/>
