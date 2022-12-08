63d287eea40764410da752f5fe586aedd8532794
zhiming <zhiming.hao@qq.com>
2022-12-08 21:04:02 +0800

    登录表单校验

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -7,38 +7,79 @@ const {Password} = Input
 
 function Login() {
 
+    /**
+     *
+     * @param values 表的所有项的值
+     */
     const onFinish = (values) => {
+        //表单校验通过之后会执行
         console.log('Success:', values);
+        //发请求登录
     };
+
+    //函数校验
+    /**
+     *
+     * @param rule  当前表单项的校验规则
+     * @param value 当前表单项的值=》checkbox的值:ture 选中 / false 未选中
+     * @return Promise
+     * 1.校验通过返回 Promise.resolve() 2.校验没通过Promise.reject(new Error('错误提示信息'))
+     */
+    const validateAgree = (rule, value) => {
+        // value ? Promise.resolve() : Promise.reject(new Error('请勾选用户协议'))
+        // console.log(rule, value)
+        if (!value) {
+            //没有勾选协议=>校验不通过该
+            return Promise.reject(new Error('请勾选协议'))
+        }
+        //校验通过
+        return Promise.resolve()
+    }
 
     return (
         <div className="login">
             <Card className="login-container">
                 {/*极客圆logo*/}
                 <img className="login-logo" src={logo} alt=""/>
-                {/* 登录表单 */}
+                {/* 登录表单 initialValues指定表单项目的默认值*/}
                 <Form
-                    name="basic"
-                    initialValues={{remember: false}}
+                    initialValues={{mobile: '', code: '', agree: false}}
+                    validateTrigger={['onBlur', 'onChange']}
                     onFinish={onFinish}
                     autoComplete="off"
                 >
+                    {/*
+                        表单校验总结:
+                        1，Form.Item组件上必须加name属性,name属性和后台接口需要的参数名保持一直
+                        2.Form.Item组件上加rules属性，添加校验规则
+                        3.通过validateTrigger指定校验规则出发的时机:['onBlur','onChange']
+                        说明:在Form组件上加validateTrigger作用域所有表单项，在自表单项的规则单独加的validateTrigger会覆盖Form组件的出发实际
+                    */}
                     <Form.Item
-                        name="username"
-                        rules={[{required: true, message: '请输入您的用户名！'}]}
+                        name="mobile"
+                        rules={[{required: true, message: '请输入您的登录手机号！'},
+                            {
+                                pattern: /^1[3-9]\d{9}$/,
+                                message: '手机号码格式不对',
+                                // 优先级最高
+                                validateTrigger: 'onBlur'
+                            },
+                        ]}
                     >
-                        <Input size='large'/>
+                        <Input size='large' placeholder='请输入手机号'/>
                     </Form.Item>
 
                     <Form.Item
-                        name="password"
-                        rules={[{required: true, message: '请输入密码！'}]}
+                        name="code"
+                        rules={[{required: true, message: '请输入验证码！'}, {len: 6, message: '请输入6位验证码'}]}
                     >
                         {/*<Input.Password size='large'/>*/}
                         <Password size='large'/>
                     </Form.Item>
-
-                    <Form.Item name="remember" valuePropName="checked">
+                    <Form.Item name="agree" valuePropName="checked" rules={[{
+                        validator: validateAgree
+                    }]}>
+                        {/*非输入框，需要加valuePropName="checked"*/}
                         <Checkbox>我已阅读并同意「用户协议」和「隐私条款」</Checkbox>
                     </Form.Item>
 
