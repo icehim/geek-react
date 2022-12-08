528e795b99f6fb6a3c0b5968a3369e00e6710e59
zhiming <zhiming.hao@qq.com>
2022-12-08 17:43:01 +0800

    登录表单静态页面完成

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,16 +1,53 @@
-import {Button} from "antd";
+import {Button, Card, Checkbox, Form, Input} from "antd";
+import logo from '@/assets/logo.png'
+import './index.scss'
 
 function Login() {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div>
-            <h1>Login</h1>
-            <div>
-                <Button type="primary">Primary Button</Button>
-                <Button>Default Button</Button>
-                <Button type="dashed">Dashed Button</Button>
-                <Button type="text">Text Button</Button>
-                <Button type="link">Link Button</Button>
-            </div>
+        <div className="login">
+            <Card className="login-container">
+                {/*极客圆logo*/}
+                <img className="login-logo" src={logo} alt=""/>
+                {/* 登录表单 */}
+                <Form
+                    name="basic"
+                    initialValues={{remember: false}}
+                    onFinish={onFinish}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{required: true, message: '请输入您的用户名！'}]}
+                    >
+                        <Input size='large'/>
+                    </Form.Item>
+
+                    <Form.Item
+                        name="password"
+                        rules={[{required: true, message: '请输入密码！'}]}
+                    >
+                        <Input.Password size='large'/>
+                    </Form.Item>
+
+                    <Form.Item name="remember" valuePropName="checked">
+                        <Checkbox>我已阅读并同意「用户协议」和「隐私条款」</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" size='large' block htmlType="submit">
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
         </div>
     );
 }
