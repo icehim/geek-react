7718b627e4ed48b1f9aa80baba81ec3214f03538
zhiming <zhiming.hao@qq.com>
2022-12-09 23:27:43 +0800

    请求头添加token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,22 @@
 import axios from "axios";
+import store from "@/store";
 
 //创建新axios实例
 const request = axios.create({
     //后台请求基础地址
     baseURL: 'http://geek.itheima.net/v1_0'
 })
+//请求拦截器
+request.interceptors.request.use((config) => {
+    //获取token
+    const {token} = store.getState()
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
 
+//响应拦截器
 //对后台数据的返回做简化
 request.interceptors.response.use((res) => {
     //直接返回data给页面
