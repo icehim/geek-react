a179f465ed6b9939e64214ee259df6ade92945ec
zhiming <zhiming.hao@qq.com>
2022-12-19 16:48:11 +0800

    404页面重构

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,7 +1,14 @@
+import {Link} from "react-router-dom";
+import styles from './index.module.scss'
+
 function NotFound() {
     return (
-        <div>
-            <h1>NotFound</h1>
+        <div className={styles.root}>
+            <h1>对不起，您访问的页面不存在~</h1>
+            <p className='back'>
+                将在 {10} 秒后，返回首页（或者：点击立即返回
+                <Link to="/home">首页</Link>）
+            </p>
         </div>
     );
 }
